21fb412262af11a88af8195844399d167c8bb7e7
RyanCShelley <94121225+RyanCShelley@users.noreply.github.com>
2022-10-27 16:05:09 -0400

    Update webpage_analyzer.py

--- webpage_analyzer.py
+++ webpage_analyzer.py
@@ -38,7 +38,7 @@ df['img_alt'] = df['img_alt'].str.replace('@@',', ')
 
 def get_img_cnt(url):
 	response = requests.get(url)
-    	soup = BeautifulSoup(response.content)
+	soup = BeautifulSoup(response.content)
     	return len(soup.find_all('img'))
 
 number_images = []
