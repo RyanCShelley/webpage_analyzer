7b103c4a0371f83a2e4ef26211c7da478b1f5a01
RyanCShelley <94121225+RyanCShelley@users.noreply.github.com>
2022-11-01 11:21:35 -0400

    Update webpage_analyzer.py

--- webpage_analyzer.py
+++ webpage_analyzer.py
@@ -17,8 +17,6 @@ uploaded_file = st.file_uploader("Choose a file")
 
 if uploaded_file is not None:
 	df = pd.read_csv(uploaded_file)
-else:
-	print ("Please Upload Your file")
 
 
 def get_img_cnt(url):
