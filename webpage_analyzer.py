18b22825087117a7ac2720fb919554c8326348e7
RyanCShelley <94121225+RyanCShelley@users.noreply.github.com>
2022-11-02 15:49:23 -0400

    Update webpage_analyzer.py

--- webpage_analyzer.py
+++ webpage_analyzer.py
@@ -276,5 +276,5 @@ st.download_button(
 
 st.subheader('Word Count')
 
-st.bar_chart(data=df, x="word count", y=None, width=0, height=0, use_container_width=True)
+st.bar_chart(data=df, x="url", y="word count", width=100%, use_container_width=True)
 
